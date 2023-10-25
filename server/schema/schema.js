af1c5d806ab0ba024a69e42ca8aaf5c7fb875fdc
Ssewagudde David <dsewagude10@gmail.com>
2023-10-26 00:11:00 +0300

    Project functionality

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -105,6 +105,12 @@ const mutation = new GraphQLObjectType({
       type: ClientType,
       args: { id: { type: GraphQLNonNull(GraphQLID) } },
       resolve(parent, args) {
+        Project.find({ clientId: args.id }).then((projects) => {
+          projects.forEach((project) => {
+            project.deleteOne();
+          });
+        });
+
         return Client.findByIdAndRemove(args.id);
       },
     },
